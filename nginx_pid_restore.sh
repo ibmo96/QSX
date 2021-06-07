287167b35bcd3f497144b5257c36135ecd50f911
ibmo96 <ibrahim.mohammad96@hotmail.com>
2021-06-07 22:57:41 +0200

    updt

--- nginx_pid_restore.sh
+++ nginx_pid_restore.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#force nginx.pid generation
+sudo pkill -9 nginx && nginx -c /etc/nginx/nginx.conf && nginx -s reload 
+
+#invalid argument for nginx.pid
 sudo mkdir /etc/systemd/system/nginx.service.d
   printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" | \
     sudo tee /etc/systemd/system/nginx.service.d/override.conf
